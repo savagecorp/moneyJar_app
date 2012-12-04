22cfbfd455bebefadf0a1ae2a0a720e5275fd3c4
savagecorp <savagecorp@gmail.com>
2012-12-04 00:25:59 -0500

    first push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoneyJarApp::Application.config.secret_token = '09ed7ba6f5c13966ecbf2693d0bafecc6d2d9c2c9ea7238b5f7a3e2aace0e9d1b431b4a6e3c4e00dc0c1b626fb92d91f9151f22997ddf09bfe240488f57ec5f8'
